f9cdbfb14cc634219c1067d1b2226c86d0bd3d4d
Martijn Staal <27222398+mastaal@users.noreply.github.com>
2023-11-22 11:52:12 +0100

    Skip test for currently unsupported shorthand citations

--- tests/test_kamerstukken.py
+++ tests/test_kamerstukken.py
@@ -109,6 +109,7 @@ class KamerstukCitationTests(unittest.TestCase):
             [KamerstukCitation("II", "2013/14", "31142", "37"), KamerstukCitation("II", "2015/16", "31142", "55"), KamerstukCitation("II", "2018/19", "35165", "4")]
         )
 
+    @unittest.skip("Multiple shorthand citations are currently not yet supported")
     def test_with_multiple_citations_shorthand(self):
         """Multiple citations in a shorthand notation. Currently not supported."""
 
